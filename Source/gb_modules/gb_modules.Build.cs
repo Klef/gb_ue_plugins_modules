9c49709614f32bf11cd2207249a915feec8c11db
Klef <klef@wasteland.su>
2022-02-02 02:38:26 +0300

    Читаемость списка модулей и заготовка под плагин

--- Source/gb_modules/gb_modules.Build.cs
+++ Source/gb_modules/gb_modules.Build.cs
@@ -8,6 +8,17 @@ public class gb_modules : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"HeadMountedDisplay",
+		});
+		// PrivateDependencyModuleNames.AddRange(new string[]
+		// {
+		// 	"InventorySystem",
+		// });
 	}
 }
