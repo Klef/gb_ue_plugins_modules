6774da15480e2effcc81fd0158ad19d7e70bab4f
Klef <klef@wasteland.su>
2022-02-04 22:44:37 +0300

    Создание С++ класса Медиа Проигрывателя

---
+++ Source/gb_modules/VideoPlane.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VideoPlane.h"
+#include "FileMediaSource.h"
+#include "MediaPlayer.h"
+
+
+// Sets default values
+AVideoPlane::AVideoPlane()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+// Called when the game starts or when spawned
+void AVideoPlane::BeginPlay()
+{
+	Super::BeginPlay();
+	if (MediaPlayer)
+	{
+		FString Path = "D:\\repo\\1984.mp4";
+		UFileMediaSource * MediaSource = NewObject<UFileMediaSource>();
+		MediaSource->FilePath = Path;
+		MediaPlayer->OpenSource(MediaSource);
+	}
+
+}
+
+
---
+++ Source/gb_modules/VideoPlane.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "VideoPlane.generated.h"
+
+
+class UMediaPlayer;
+
+UCLASS()
+class GB_MODULES_API AVideoPlane : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AVideoPlane();
+	
+	UPROPERTY(EditAnywhere)
+	UMediaPlayer * MediaPlayer;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	//virtual void Tick(float DeltaTime) override;
+};
